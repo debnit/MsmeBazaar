97260ecee8f3b26b7bce6117a76601fab78e08d3
debnit <debasis.nitrkl@gmail.com>
2025-07-23 23:40:00 +0000

    pdf-generation

--- apps/web/lib/pdf-utils.ts
+++ apps/web/lib/pdf-utils.ts
@@ -2,6 +2,6 @@ import { pdf } from '@react-pdf/renderer';
 import AdminReportPDF from "@/components/pdf/admin-report-pdf";
 
 export const generateAdminPDF = async (data: any) => {
-  const buffer = await pdf(<AdminReportPDF data={data} />).toBlob();
+  const buffer = await pdf(React.createElement(AdminReportPDF, { data })).toBlob();
   return buffer;
 };
