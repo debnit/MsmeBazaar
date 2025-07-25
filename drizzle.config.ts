cdf576fc268ec7bc107d9bea163a848ebfebd4d7
debnit <debasis.nitrkl@gmail.com>
2025-07-25 18:51:38 +0000

    chore: init prisma schema and migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
+  throw new Error("DATABASE_URL not set in .env");
 }
 
 export default defineConfig({
-  out: "./migrations",
-  schema: "./shared/schema.ts",
+  out: "./libs/db/migrations",
+  schema: "./libs/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
